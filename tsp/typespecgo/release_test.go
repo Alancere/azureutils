bb2aaf843b1f54cc83e3912a821960ad0c511364
Alancere <804873052@qq.com>
2024-07-23 15:40:26 +0800

    generate by autoret.go

--- tsp/typespecgo/release_test.go
+++ tsp/typespecgo/release_test.go
@@ -9,15 +9,22 @@ import (
 	"testing"
 
 	"github.com/Alancere/azureutils/common"
+	"github.com/Alancere/azureutils/mergego"
 	"github.com/Alancere/azureutils/tsp/typespecgo"
 	"github.com/goccy/go-yaml"
 )
 
 // test generator release-v2 tsp
 func TestGenerateTool_Support_TSP(t *testing.T) {
+	// 存放通过typespec-go生成的结果
 	rootDir := "D:/Go/src/github.com/Azure"
 	specDir := "D:/Go/src/github.com/Azure/azure-rest-api-specs"
 	sdkDir := "D:/Go/src/github.com/Azure/azure-sdk-for-go"
+
+	// 存放通过autorset.go生成的结果
+	debugSdkDir := "D:/Go/src/github.com/Azure/debug/azure-sdk-for-go"
+	debugSpecDir := "D:/Go/src/github.com/Azure/debug/azure-rest-api-specs"
+	autorestGenerate := false // 是否通过autorest.go生成
 
 	genertorErrs := make([]error, 0)
 	goVetErrs := make([]error, 0)
@@ -140,7 +147,7 @@ func TestGenerateTool_Support_TSP(t *testing.T) {
 		)
 		// go mod tidy and go vet ./...
 		if err != nil {
-			log.Printf("tsp-sdk###%s %s\n", tspConfig.Path, output)
+			log.Printf("tsp-sdk error###%s %s\n", tspConfig.Path, output)
 			genertorErrs = append(genertorErrs, err)
 			continue
 		}
@@ -151,11 +158,63 @@ func TestGenerateTool_Support_TSP(t *testing.T) {
 			log.Println("govet###", output)
 			goVetErrs = append(goVetErrs, err)
 			continue
+		} else {
+			// merge go files
+			if err = mergego.Merge(tspsdk, filepath.Join("D:/tmp/typespecp-tool-diff", filepath.Base(tspsdk)+".go"), false); err != nil {
+				log.Fatal(err)
+			}
+
+			// merge fake go files
+			if err = mergego.Merge(filepath.Join(tspsdk, "fake"), filepath.Join("D:/tmp/typespecp-tool-diff", filepath.Base(tspsdk)+"_fake.go"), false); err != nil {
+				log.Fatal(err)
+			}
 		}
 		_ = output
 
 		successedTSP = append(successedTSP, configPath)
 		// break
+
+		// autorest generate go sdk
+		if autorestGenerate {
+			autorestsdk := filepath.Join(debugSdkDir, moduleName.(string))
+			serviceName, armServiceName := armName(moduleName.(string))
+			specName := filepath.Base(filepath.Join(filepath.Dir(configPath), "../"))
+			autorestOps := []string{
+				"release-v2",
+				debugSdkDir,
+				debugSpecDir,
+				serviceName, armServiceName,
+				fmt.Sprintf("--spec-rp-name=%s", specName),
+				"--skip-generate-example",
+				"--skip-create-branch",
+			}
+			defaultTag, err := readmemd(filepath.Join(filepath.Dir(configPath), "../resource-manager/readme.md"))
+			if err != nil {
+				log.Println(err)
+			}
+			if defaultTag != "" {
+				autorestOps = append(autorestOps, fmt.Sprintf("--package-config=%s", strings.TrimSpace(defaultTag)))
+			}
+			output, err := common.Generate(filepath.Join(debugSdkDir, "../"), autorestOps...)
+			if err != nil {
+				log.Println("##autorest##", err)
+			} else {
+				if _, err = common.Go(autorestsdk, "vet", "./..."); err != nil {
+					log.Println("##autorest##go vet", err)
+				} else {
+					// merge go files
+					if err = mergego.Merge(autorestsdk, filepath.Join("D:/tmp/autorest-diff", filepath.Base(autorestsdk)+".go"), false); err != nil {
+						log.Fatal(err)
+					}
+
+					// merge fake go files
+					if err = mergego.Merge(filepath.Join(autorestsdk, "fake"), filepath.Join("D:/tmp/autorest-diff", filepath.Base(autorestsdk)+"_fake.go"), false); err != nil {
+						log.Fatal(err)
+					}
+				}
+			}
+			_ = output
+		}
 	}
 
 	fmt.Println("successed tsp count:", len(successedTSP))
@@ -182,6 +241,22 @@ func TestGenerateTool_Support_TSP(t *testing.T) {
 	for _, err := range goVetErrs {
 		fmt.Println(err)
 	}
+}
+
+func readmemd(path string) (string, error) {
+	md, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	for _, l := range strings.Split(string(md), "\n") {
+		// get first tag
+		if strings.Contains(l, "tag:") {
+			return l, nil
+		}
+	}
+
+	return "", nil
 }
 
 func readmegomd(path string) map[string]any {
